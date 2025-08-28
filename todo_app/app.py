8fe05e8699d383e35b3b1b8e0ab7bc3076f69ac8
Patrik WM <15982423+patrikwm@users.noreply.github.com>
2025-08-28 10:51:39 +0200

    Update README and application for version 1.5: add new deployment details and enhance API response

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,9 @@
-from flask import Flask
 import logging
 import os
+import random
+import string
+
+from flask import Flask
 
 app = Flask(__name__)
 logging.basicConfig(
@@ -10,11 +13,22 @@ logging.basicConfig(
 
 FLASK_PORT = int(os.environ.get("PORT", 8080))
 
+
+def generate_random_string(length=10):
+    """Generate a random string of fixed length."""
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for i in range(length))
+
+APP_HASH = generate_random_string(6)
+
 @app.route('/')
 def home():
-    return "Welcome to the To-Do List API!"
+    home_return_string = "<strong>App instance hash:</strong> {0}<br>".format(APP_HASH)
+    home_return_string += "<strong>User request hash:</strong> {0}\n".format(generate_random_string(6))
+    return home_return_string
 
 # Default port:
 if __name__ == '__main__':
     logging.info("Server started in port {0}".format(FLASK_PORT))
+    logging.info("App instance hash: {0}".format(APP_HASH))
     app.run(host='0.0.0.0', port=FLASK_PORT)
