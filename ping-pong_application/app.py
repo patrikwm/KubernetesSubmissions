3a78be90c8abeec6e2bd2c5d004f2d6fd0f7fe4f
Patrik WM <15982423+patrikwm@users.noreply.github.com>
2025-08-28 14:57:34 +0200

    Remove unused imports and functions from app.py

--- ping-pong_application/app.py
+++ ping-pong_application/app.py
@@ -1,10 +1,9 @@
 # This application generates a random string and echoes it with a UTC timestamp to standard output.
 import logging
-import string
 from time import sleep
 from datetime import datetime, timezone
 
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 FLASK_PORT = 3000
@@ -15,17 +14,6 @@ logging.basicConfig(
     )
 
 PING_PONG_COUNTER = 0
-
-def generate_random_string(length=10):
-    """Generate a random string of fixed length."""
-    letters = string.ascii_letters + string.digits
-    return ''.join(random.choice(letters) for i in range(length))
-
-def get_current_timestamp():
-    """Get the current UTC timestamp in the required format."""
-    now = datetime.now(timezone.utc)
-    return now.strftime("%Y-%m-%dT%H:%M:%S") + f".{now.microsecond // 1000:03d}Z"
-
 
 @app.route('/pingpong')
 def get_status():
